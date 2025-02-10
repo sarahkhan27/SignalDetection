519ed8ffbe72da4abe3ed890bdc9b959c49dc747
khans5 <sk@class-docker>
2025-02-10 06:17:15 +0000

    new code

---
+++ signal_detection.py
@@ -0,0 +1,7 @@
+class SignalDetection:
+    def __init__(self, hits, misses, falseAlarms, correctRejections):
+        # Initialize the instance variables
+        self.hits = hits
+        self.misses = misses
+        self.falseAlarms = falseAlarms
+        self.correctRejections = correctRejections
